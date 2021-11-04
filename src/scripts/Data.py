a970d1ab434b92b0f7cb35531795083c87ebd7be
Fernando Perez <fernando.perez.lara@outlook.com>
2021-11-04 11:57:26 +0100

    Division of data into training and testing

--- src/scripts/Data.py
+++ src/scripts/Data.py
@@ -1,9 +1,27 @@
 import os
 import pandas as pd
+import numpy as np
+from sklearn import model_selection
 
 class Data:
 	def __init__(self, path):
 		self.images = self.__extract_images(path)
+		self.images.category, self.labels = self.images.category.factorize()
+		self.training, self.test = None, None
+
+	def train_test_split(self, test_size=0.15, shuffle=True, stratify=False):
+		self.training, self.test = model_selection.train_test_split(
+			self.images,
+			test_size=test_size,
+			random_state=42,
+			shuffle=shuffle,
+			stratify=self.images.category if stratify else None
+		);
+
+	def count_labels(self, data, name):
+		amount = data.category.value_counts().values
+		
+		print(f"{name}: {amount} {np.round(amount/len(data), 2)}")
 
 	def __extract_images(self, path):
 		images = []
--- src/scripts/Misc.py
+++ src/scripts/Misc.py
@@ -1,4 +1,3 @@
-from abc import abstractmethod
 import tensorflow as tf
 
 class computer:
