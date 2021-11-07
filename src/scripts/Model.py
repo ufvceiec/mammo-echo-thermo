e23fc49f7947efca85044ea67e91ecbf670eba64
Fernando Perez <fernando.perez.lara@outlook.com>
2021-11-07 17:14:47 +0100

    Model compilation and training

--- src/scripts/Model.py
+++ src/scripts/Model.py
@@ -1,10 +1,23 @@
 import matplotlib.pyplot as plt
-from keras import models, layers, utils
+from keras import callbacks, models, layers, utils, losses
+from tensorflow.keras import optimizers
 from .Misc import *
 
 class Model:
-	def __init__(self):
+	def __init__(self, name, summary=True, plot=True):
 		self.model = self.__create_model()
+		self.name = name
+
+		if summary:
+			self.model.summary()
+
+		if plot:
+			computer.create_output_folder()
+
+			utils.vis_utils.plot_model(self.model, to_file=f"./output/{self.name}_model.png", show_shapes=True, show_layer_names=True)
+
+			plt.imshow(plt.imread(f"./output/{self.name}_model.png"))
+			plt.show()		
 
 	def __create_model(self):
 		model = models.Sequential()
@@ -18,15 +31,34 @@ class Model:
 		model.add(layers.Dropout(rate=0.4))
 		model.add(layers.Dense(32, activation="relu"))
 		model.add(layers.Dropout(rate=0.4))
-		model.add(layers.Dense(2, activation="softmax"))
+		model.add(layers.Dense(1, activation="softmax"))
 
 		return model
 
-	def plot(self, name):
-		computer.create_output_folder()
+	def compile(self):
+		self.model.compile(loss=losses.binary_crossentropy, optimizer=optimizers.Adam(learning_rate=5*10e-5), metrics=["accuracy"])
 
-		utils.vis_utils.plot_model(self.model, to_file="./output/" + name + ".png", show_shapes=True, show_layer_names=True)
+		with open(f"./output/{self.name}_model.json", "w") as json_file:
+			json_file.write(self.model.to_json())
 
-		plt.imshow(plt.imread("./output/" + name + ".png"))
+	def fit(self, train_generator, validation_generator, epochs, plot=True):
+		checkpoint = callbacks.ModelCheckpoint(f"./output/{self.name}_weigth.hdf5", monitor="val_accuracy", verbose=1, save_best_only=True, mode="max")
 
-		plt.show()
+		history = self.model.fit(train_generator, epochs=epochs, validation_data=validation_generator, callbacks=[checkpoint])
+
+		if plot:
+			plt.style.use("ggplot")
+
+			plt.figure()
+
+			plt.plot(history.history["loss"], label="Training loss")
+			plt.plot(history.history["val_loss"], label="Validation loss")
+			plt.plot(history.history["accuracy"], label="Training accuracy")
+			plt.plot(history.history["val_accuracy"], label="Validation accuracy")
+
+			plt.title("Training Loss and Accuracy")
+			plt.xlabel("Epoch #")
+			plt.ylabel("Loss/Accuracy")
+			plt.legend(loc="lower left")
+
+			plt.show()
