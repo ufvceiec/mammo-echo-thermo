d99a8d42ad7b7ed62fd6b97b6ca8770c8efaecb7
Fernando Perez <fernando.perez.lara@outlook.com>
2021-11-08 12:20:31 +0100

    Grad-CAM is applied to the model

--- src/scripts/Model.py
+++ src/scripts/Model.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
-from keras import callbacks, models, layers, utils, losses
+import cv2 as cv
+import tensorflow as tf
+from keras import callbacks, models, layers, utils, losses, preprocessing
 from sklearn import metrics
 from tensorflow.keras import optimizers
 from .Misc import *
@@ -96,3 +99,55 @@ class Model:
 
 		precision = (TP / float(TP + FP))
 		print("Precision:", round(precision, 4))
+
+	def visualize_heatmap(self, image):
+		img = cv.cvtColor(cv.imread(image.image), cv.COLOR_BGR2RGB).astype("float32") * 1./255
+		img = np.expand_dims(img, axis=0)
+
+		heatmap = self.__compute_heatmap(img)
+		jet_heatmap, superimposed_img = self.__get_heatmap(image.image, heatmap)
+
+		fig, ax = plt.subplots(1, 3, figsize=(20, 8))
+
+		ax[0].imshow(img[0])
+		ax[0].set_title(os.path.basename(image.image))
+		ax[1].imshow(preprocessing.image.array_to_img(jet_heatmap))
+		ax[1].set_title(f"Real class: {image.category}")
+		ax[2].imshow(superimposed_img)
+		ax[2].set_title(f"Predicted class: {np.argmax(self.model.predict(img)[0])}")
+
+		plt.show()
+
+	def __compute_heatmap(self, image):
+		last_layer = self.model.get_layer("conv2d_3")
+
+		grad_model = models.Model(inputs=[self.model.input], outputs=[last_layer.output, self.model.output])
+
+		with tf.GradientTape() as tape:
+			model_output, last_layer = grad_model(image)
+
+			grads = tape.gradient(last_layer[:, 1], model_output)
+			pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+		heatmap = model_output[0] @ pooled_grads[..., tf.newaxis]
+		heatmap = tf.squeeze(heatmap)
+		heatmap = tf.maximum(heatmap, 0)/tf.math.reduce_max(heatmap)
+
+		return heatmap.numpy()
+
+	def __get_heatmap(self, path, heatmap):
+		img = preprocessing.image.load_img(path, color_mode="grayscale")
+		img = preprocessing.image.img_to_array(img)
+
+		jet = cm.get_cmap("jet")
+		jet_colors = jet(np.arange(256))[:, :3]
+		
+		jet_heatmap = jet_colors[np.uint8(255 * heatmap)]
+		jet_heatmap = preprocessing.image.array_to_img(jet_heatmap)
+		jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+		jet_heatmap = preprocessing.image.img_to_array(jet_heatmap)
+
+		superimposed_img = jet_heatmap * 0.4 + img
+		superimposed_img = preprocessing.image.array_to_img(superimposed_img)
+
+		return jet_heatmap, superimposed_img
