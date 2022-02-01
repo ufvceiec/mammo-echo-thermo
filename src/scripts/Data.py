cb05f6f294ed87261b8ba7f0ef86046b94447fd9
Fernando Perez <fernando.perez.lara@outlook.com>
2022-02-01 11:05:30 +0100

    Cross-validation has been applied to the training data

--- src/scripts/Data.py
+++ src/scripts/Data.py
@@ -8,33 +8,74 @@ from keras import preprocessing
 from .Misc import *
 
 class Data:
-	def __init__(self, path):
+	def __init__(self, path, random_state=42):
+		self.random_state = random_state
+		self.path = path
 		self.images = self.__extract_images(path) # The paths of the images and their categories are loaded into a dataframe
 		self.images.category, self.labels = self.images.category.factorize() # Categories are converted to integers and the labels are stored in a dictionary
 		self.images.category = self.images.category.astype(str)
-		self.random_state = 42
 		self.training, self.test = None, None
 
 	# Split data into train and test
-	def train_test_split(self, test_size=0.15, random_state=42, stratify=False):
-		self.random_state = random_state
-
+	def train_test_split(self, test_size=0.15, stratify=False):
 		return model_selection.train_test_split(
 			self.images,
 			test_size=test_size,
-			random_state=random_state,
+			random_state=self.random_state,
 			shuffle=True,
 			stratify=self.images.category if stratify else None
 		)
 
-	def count_labels(self, data, name):
+	# The number of each class in the dataset is counted
+	def count_labels(self, data, name="Dataset"):
 		amount = data.category.value_counts().values
 		
 		print(f"{name}: {amount} {np.round(amount/len(data), 2)}")
 
-	def image_generator(self, shuffle=True):
-		train_datagen = preprocessing.image.ImageDataGenerator(rescale=1./255, validation_split=0.2)
-		test_datagen = preprocessing.image.ImageDataGenerator(rescale=1./255)
+	# Function in charge of generating training and validation images
+	def training_validation_generator(self, n_splits=5):
+		kfold = model_selection.KFold(n_splits=n_splits, shuffle=True, random_state=self.random_state) # Split the data into n_splits folds
+		datagen, generator_properties = self.generator() # Generate the generator and its properties
+
+		# For each fold, generate the training and validation images
+		for training_index, validation_index in kfold.split(self.training):
+			training_data = self.training.iloc[training_index]
+			validation_data = self.training.iloc[validation_index]
+
+			train_generator = datagen.flow_from_dataframe(
+				**generator_properties,
+
+				dataframe=training_data,
+				batch_size=10,
+				shuffle=False,
+			)
+
+			validation_generator = datagen.flow_from_dataframe(
+				**generator_properties,
+
+				dataframe=validation_data,
+				batch_size=10,
+				shuffle=False,
+			)
+
+			yield train_generator, validation_generator
+
+	# Function in charge of generating test images
+	def test_generator(self):
+		datagen, generator_properties = self.generator() # Generate the generator and its properties
+
+		test_generator = datagen.flow_from_dataframe(
+			**generator_properties,
+
+			dataframe=self.test,
+			batch_size=1,
+			shuffle=False
+		)
+
+		return test_generator
+
+	def generator(self):
+		datagen = preprocessing.image.ImageDataGenerator(rescale=1./255)
 
 		generator_properties = {
 			"x_col": "image",
@@ -44,33 +85,7 @@ class Data:
 			"class_mode": "categorical"
 		}
 
-		train_generator = train_datagen.flow_from_dataframe(
-			**generator_properties,
-
-			dataframe=self.training,
-			batch_size=10,
-			shuffle=shuffle,
-			subset="training"
-		)
-
-		validation_generator = train_datagen.flow_from_dataframe(
-			**generator_properties,
-
-			dataframe=self.training,
-			batch_size=10,
-			shuffle=shuffle,
-			subset="validation"
-		)
-
-		test_generator = test_datagen.flow_from_dataframe(
-			**generator_properties,
-
-			dataframe=self.test,
-			batch_size=1,
-			shuffle=False
-		)
-
-		return train_generator, validation_generator, test_generator
+		return datagen, generator_properties
 
 	def detectColor(self, image, lower, upper):
 		if tf.is_tensor(image):
