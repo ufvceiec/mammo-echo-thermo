3a9747b1f0e36ab4a0a9f4cac9022b4a061c1bca
Fernando Perez <fernando.perez.lara@outlook.com>
2022-02-01 10:14:14 +0100

    Added option to ignore GPUs availability check

--- src/scripts/Misc.py
+++ src/scripts/Misc.py
@@ -1,16 +1,15 @@
-from copy import copy
 import tensorflow as tf
 import os
 import shutil
 
 class computer:
 	# This function checks if the model is running on the CPU or GPU
-	def check_available_devices():
+	def check_available_devices(ignore=False):
 		print("Available devices:")
 		for device in tf.config.experimental.list_physical_devices():
 			print(f"- {device.device_type}: {device.name}")
 
-		if len(tf.config.list_physical_devices("GPU")) == 0:
+		if (ignore == False) and (len(tf.config.list_physical_devices("GPU")) == 0):
 			print("\nCannot run model on GPU. Do you want to run it with the CPU? [Y/N]: ")
 			if input().upper() != "Y":
 				raise SystemExit("Execution of the model has been canceled!")
