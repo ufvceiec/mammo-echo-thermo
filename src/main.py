8c1b1b0dcf26ef315723fc340b47e8b530d68d80
Fernando Perez <fernando.perez.lara@outlook.com>
2022-02-10 13:59:43 +0100

    RGB filters created

--- src/main.py
+++ src/main.py
@@ -17,40 +17,40 @@ test_generator = data.test_generator()
 
 filters = {
 	"original": lambda x: x,
-	"high": lambda x: data.get_image_tensor(x, (330, 0, 0), (360, 255, 255)) + data.get_image_tensor(x, (0, 0, 0), (60, 255, 255)),
-	"medium": lambda x: data.get_image_tensor(x, (60, 0, 0), (130, 255, 255)),
-	"low": lambda x: data.get_image_tensor(x, (130, 0, 0), (330, 255, 255))
+	"red": lambda x: x[:, :, :, 0][..., None],
+	"green": lambda x: x[:, :, :, 1][..., None],
+	"blue": lambda x: x[:, :, :, 2][..., None]
 }
 
-high_model = [Model(path=f"./output/state_{random_state}/high/fold_{index}", filter=filters["high"]) for index in range(len(training_validation_generator))]
-medium_model = [Model(path=f"./output/state_{random_state}/medium/fold_{index}", filter=filters["medium"]) for index in range(len(training_validation_generator))]
-low_model = [Model(path=f"./output/state_{random_state}/low/fold_{index}", filter=filters["low"]) for index in range(len(training_validation_generator))]
+red_model = [Model(path=f"./output/state_{random_state}/red/fold_{index}", filter=filters["red"]) for index in range(len(training_validation_generator))]
+green_model = [Model(path=f"./output/state_{random_state}/green/fold_{index}", filter=filters["green"]) for index in range(len(training_validation_generator))]
+blue_model = [Model(path=f"./output/state_{random_state}/blue/fold_{index}", filter=filters["blue"]) for index in range(len(training_validation_generator))]
 
-[high_model[index].compile() for index in range(len(high_model))]
-[medium_model[index].compile() for index in range(len(medium_model))]
-[low_model[index].compile() for index in range(len(low_model))]
+[red_model[index].compile() for index in range(len(red_model))]
+[green_model[index].compile() for index in range(len(green_model))]
+[blue_model[index].compile() for index in range(len(blue_model))]
 
-[high_model[index].fit(training_validation_generator[index][0], training_validation_generator[index][1], epochs=600, verbose=False) for index in range(len(high_model))]
-[medium_model[index].fit(training_validation_generator[index][0], training_validation_generator[index][1], epochs=600, verbose=False) for index in range(len(medium_model))]
-[low_model[index].fit(training_validation_generator[index][0], training_validation_generator[index][1], epochs=600, verbose=False) for index in range(len(low_model))]
+[red_model[index].fit(training_validation_generator[index][0], training_validation_generator[index][1], epochs=600, verbose=False) for index in range(len(red_model))]
+[green_model[index].fit(training_validation_generator[index][0], training_validation_generator[index][1], epochs=600, verbose=False) for index in range(len(green_model))]
+[blue_model[index].fit(training_validation_generator[index][0], training_validation_generator[index][1], epochs=600, verbose=False) for index in range(len(blue_model))]
 
-for index in range(len(high_model)):
-	high_model[index].evaluate(training_validation_generator[index][0], title="train_generator", path=None)
-	high_model[index].evaluate(training_validation_generator[index][1], title="validation_generator", path=None)
-	high_model[index].evaluate(test_generator, title="test_generator", path=None)
+for index in range(len(red_model)):
+	red_model[index].evaluate(training_validation_generator[index][0], title="train_generator", path=None)
+	red_model[index].evaluate(training_validation_generator[index][1], title="validation_generator", path=None)
+	red_model[index].evaluate(test_generator, title="test_generator", path=None)
 
-for index in range(len(medium_model)):
-	medium_model[index].evaluate(training_validation_generator[index][0], title="train_generator", path=None)
-	medium_model[index].evaluate(training_validation_generator[index][1], title="validation_generator", path=None)
-	medium_model[index].evaluate(test_generator, title="test_generator", path=None)
+for index in range(len(green_model)):
+	green_model[index].evaluate(training_validation_generator[index][0], title="train_generator", path=None)
+	green_model[index].evaluate(training_validation_generator[index][1], title="validation_generator", path=None)
+	green_model[index].evaluate(test_generator, title="test_generator", path=None)
 
-for index in range(len(low_model)):
-	low_model[index].evaluate(training_validation_generator[index][0], title="train_generator", path=None)
-	low_model[index].evaluate(training_validation_generator[index][1], title="validation_generator", path=None)
-	low_model[index].evaluate(test_generator, title="test_generator", path=None)
+for index in range(len(blue_model)):
+	blue_model[index].evaluate(training_validation_generator[index][0], title="train_generator", path=None)
+	blue_model[index].evaluate(training_validation_generator[index][1], title="validation_generator", path=None)
+	blue_model[index].evaluate(test_generator, title="test_generator", path=None)
 
-join_models = [Join(high_model[index], medium_model[index], low_model[index], path=f"./output/state_{random_state}/join/fold_{index}") for index in range(len(high_model))]
+join_models = [Join(red_model[index], green_model[index], blue_model[index], path=f"./output/state_{random_state}/join/fold_{index}") for index in range(len(red_model))]
 
-[join_models[index].get_weighted_average(test_generator, iterations=100, tolerance=1e-7) for index in range(len(high_model))]
+[join_models[index].get_weighted_average(test_generator, iterations=100, tolerance=1e-7) for index in range(len(red_model))]
 
-[join_models[index].evaluate(test_generator, title="test_generator") for index in range(len(high_model))]
+[join_models[index].evaluate(test_generator, title="test_generator") for index in range(len(red_model))]
